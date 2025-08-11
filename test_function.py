90a18c2ae12a2d2244773e09c9e2f1e1aa520eed
Melek Nur <meleknurbacakli@gmail.com>
2025-08-11 21:02:55 +0300

    Add inference script for sequence-to-sequence translation
    - Loads trained encoder-decoder model from training_model.h5
    - Builds separate encoder and decoder inference models
    - Implements decode_sequence function for generating translations
    - Adds preprocessing for custom input sentences
    - Includes example translations for sample sentences

---
+++ test_function.py
@@ -0,0 +1,100 @@
+from preprocessing import input_features_dict, target_features_dict, reverse_input_features_dict, reverse_target_features_dict, max_decoder_seq_length, input_docs, target_docs, input_tokens, target_tokens
+from training_model import encoder_inputs, decoder_inputs, encoder_states, decoder_lstm, decoder_dense, encoder_input_data, num_decoder_tokens, latent_dim
+
+from tensorflow import keras
+from tensorflow.keras.layers import Input # type: ignore
+from keras.models import Model, load_model # type: ignore
+import numpy as np
+
+training_model = load_model('training_model.h5')
+encoder_inputs = training_model.input[0]
+encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
+encoder_states = [state_h_enc, state_c_enc]
+
+encoder_model = Model(encoder_inputs, encoder_states)
+
+
+decoder_state_input_hidden = Input(shape=(latent_dim,))
+decoder_state_input_cell = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+decoder_outputs, state_hidden, state_cell = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+
+def decode_sequence(test_input):
+  # Encode the input as state vectors.
+  states_value = encoder_model.predict(test_input)
+
+  # Generate empty target sequence of length 1.
+  target_seq = np.zeros((1, 1, num_decoder_tokens))
+  # Populate the first token of target sequence with the start token.
+  target_seq[0, 0, target_features_dict['<START>']] = 1.
+
+  # Sampling loop for a batch of sequences
+  # (to simplify, here we assume a batch of size 1).
+  decoded_sentence = ''
+
+  stop_condition = False
+  while not stop_condition:
+    # Run the decoder model to get possible 
+    # output tokens (with probabilities) & states
+    output_tokens, hidden_state, cell_state = decoder_model.predict(
+      [target_seq] + states_value)
+
+    # Choose token with highest probability
+    sampled_token_index = np.argmax(output_tokens[0, -1, :])
+    sampled_token = reverse_target_features_dict[sampled_token_index]
+    decoded_sentence += " " + sampled_token
+
+    # Exit condition: either hit max length
+    # or find stop token.
+    if (sampled_token == '<END>' or len(decoded_sentence) > max_decoder_seq_length):
+      stop_condition = True
+
+    # Update the target sequence (of length 1).
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    target_seq[0, 0, sampled_token_index] = 1.
+
+    # Update states
+    states_value = [hidden_state, cell_state]
+
+  return decoded_sentence
+
+# CHANGE RANGE (NUMBER OF TEST SENTENCES TO TRANSLATE) AS YOU PLEASE
+for seq_index in range(10):
+  test_input = encoder_input_data[seq_index: seq_index + 1]
+  decoded_sentence = decode_sequence(test_input)
+  print('-')
+  print('Input sentence:', input_docs[seq_index])
+  print('Decoded sentence:', decoded_sentence)
+
+def preprocess_input_sentence(sentence):
+  tokens = sentence.split()
+  input_seq = []
+  for token in tokens:
+    if token in input_features_dict:
+      input_seq.append(input_features_dict[token])
+    else:
+      input_seq.append(input_features_dict.get('<UNK>', 0))
+
+  input_array = np.zeros((1, len(input_seq), len(input_features_dict)), dtype='float32')
+  for t, idx in enumerate(input_seq):
+    input_array[0, t, idx] = 1.
+  return input_array
+
+if __name__ == '__main__':
+  test_sentences = [
+    "Angels are beautiful",
+    "I love you",
+    "This is a test sentence.",
+    "Can you help me?",
+    "What time is it?"
+    ]
+
+for sentence in test_sentences:
+  test_input = preprocess_input_sentence(sentence)
+  translation = decode_sequence(test_input)
+  print(f"Input: {sentence}")
+  print(f"Translated: {translation}")
+  print('-' * 30)
